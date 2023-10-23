e65b3a8ce3d26bbca5f6a28406d80c8a9738c97a
Yuriy Maksyuta <yura-maksuta@mail.ru>
2023-10-23 22:12:40 +0300

    Kaggle change path

--- hw_asr/datasets/librispeech_dataset.py
+++ hw_asr/datasets/librispeech_dataset.py
@@ -63,7 +63,7 @@ class LibrispeechDataset(BaseDataset):
 
     def _create_index(self, part):
         index = []
-        split_dir = Path("kaggle/input").resolve() / part / 'LibriSpeech' / part
+        split_dir = Path("kaggle/input/librispeech") / part / 'LibriSpeech' / part
         # split_dir = self._data_dir / part
         if not split_dir.exists():
             self._load_part(part)

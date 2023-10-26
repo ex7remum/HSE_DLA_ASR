d741e810f0867c681ca5c6897e43320eddfd696a
Yuriy Maksyuta <yura-maksuta@mail.ru>
2023-10-26 21:11:01 +0300

    Fix download

--- get_model.py
+++ get_model.py
@@ -1,20 +1,21 @@
-import os, wget
+import os
+import gdown
 
 if __name__ == "__main__":
     model_url = 'https://drive.google.com/file/d/1KYa__v68ofjDiMRJB4UzZBizC-zi5cIk/view?usp=sharing'
-    model_path = 'model.pth'
+    model_path = 'best_model/model.pth'
     if not os.path.exists(model_path):
         print('Downloading DeepSpeech2 model.')
-        model_path = wget.download(model_url)
+        gdown.download(model_url, model_path)
         print('Downloaded DeepSpeech2 model.')
     else:
         print('Deepspeech2 model already exists.')
 
     config_url = 'https://drive.google.com/file/d/1neW1hfHfJ1XNdJ4ztWHSAE62_GFdkAkm/view?usp=sharing'
-    config_path = 'config.json'
+    config_path = 'best_model/config.json'
     if not os.path.exists(config_path):
         print('Downloading DeepSpeech2 model test config.')
-        config_path = wget.download(config_url)
+        gdown.download(config_url, config_path)
         print('Downloaded DeepSpeech2 model test config.')
     else:
         print('Deepspeech2 model config already exists.')
